89618d0cf1cbc4f7f7a0cacf4c931e892407930e
canopeerus <visvanathannaditya@gmail.com>
2022-10-22 12:04:28 -0600

    spring initializr bootstrap

---
+++ src/main/java/com/teksecure/service/impoundsrv/ImpoundsrvApplication.java
@@ -0,0 +1,13 @@
+package com.teksecure.service.impoundsrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpoundsrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpoundsrvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teksecure/service/impoundsrv/ImpoundsrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teksecure.service.impoundsrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpoundsrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
