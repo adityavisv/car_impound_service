20ab7987c80d6041f30b6e1ae124bd31bd414ff7
canopeerus <visvanathannaditya@gmail.com>
2022-10-24 16:34:56 -0600

    add parking spot db schema

--- dbschema.sql
+++ dbschema.sql
@@ -28,3 +28,14 @@ CREATE TABLE `owner` (
                          `CONTACT_NUM` varchar(15) DEFAULT NULL,
                          PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `parking_zone` (
+                                `ZONE_LABEL` varchar(2) DEFAULT NULL,
+                                `SLOT_NUM` int DEFAULT NULL,
+                                `OC_STATUS` varchar(20) DEFAULT NULL,
+                                `VEHICLE_ID` int DEFAULT NULL,
+                                `ID` int NOT NULL AUTO_INCREMENT,
+                                PRIMARY KEY (`ID`),
+                                KEY `vehicle_id_fk` (`VEHICLE_ID`),
+                                CONSTRAINT `vehicle_id_fk` FOREIGN KEY (`VEHICLE_ID`) REFERENCES `vehicles` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
