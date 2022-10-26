22ada8ba13e529590a3fd81dac381ab8fe2d9b24
canopeerus <visvanathannaditya@gmail.com>
2022-10-25 23:49:00 -0600

    Add basic auth security to the service

--- dbschema.sql
+++ dbschema.sql
@@ -38,4 +38,18 @@ CREATE TABLE `parking_zone` (
                                 PRIMARY KEY (`ID`),
                                 KEY `vehicle_id_fk` (`VEHICLE_ID`),
                                 CONSTRAINT `vehicle_id_fk` FOREIGN KEY (`VEHICLE_ID`) REFERENCES `vehicles` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `users` (
+                         `username` varchar(50) NOT NULL,
+                         `password` varchar(100) NOT NULL,
+                         `enabled` tinyint NOT NULL DEFAULT '1',
+                         PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `authorities` (
+                               `username` varchar(50) NOT NULL,
+                               `authority` varchar(50) NOT NULL,
+                               UNIQUE KEY `ix_auth_username` (`username`,`authority`),
+                               CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ;
